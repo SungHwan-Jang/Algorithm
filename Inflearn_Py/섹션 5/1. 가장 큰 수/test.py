9aa324e3a5393aff4f602b3ae53ead7fac1b9d18
Hwan <redmk1025@gmail.com>
2020-05-01 20:21:58 +0900

    fin 5-1

---
+++ Inflearn_Py/섹션 5/1. 가장 큰 수/test.py
@@ -0,0 +1,49 @@
+import sys
+
+sys.stdin = open('test.txt', 'rt')
+sample, m = map(str, input().split())
+sample = list(sample)
+m = int(m)
+
+for i in range(len(sample)):
+    sample[i] = int(sample[i])
+
+res = []
+cnt = 0
+
+res.append(sample[0])
+
+for i in range(1, len(sample)):
+    if cnt == m:
+        res.append(sample[i])
+        continue
+
+    before = res.pop()
+    if before < sample[i]:
+        while True:
+            try:
+                if before < sample[i]:
+                    cnt += 1
+
+                    if cnt == m:
+                        res.append(sample[i])
+                        break
+
+                    before = res.pop()
+                    continue
+                else:
+                    res.append(before)
+                    res.append(sample[i])
+                    break
+            except:
+                res.append(sample[i])
+                break
+    else:
+        res.append(before)
+        res.append(sample[i])
+
+for _ in range(m-cnt):
+    res.pop()
+
+for val in res:
+    print(val, end=' ')
