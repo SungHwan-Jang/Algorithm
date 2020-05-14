bec696b3387b043a18b350fceda00e754489aa9d
SungHwan-Jang <redmk1025@gmail.com>
2020-05-14 17:29:15 +0900

    fin 6 - 11
    6-11

--- Inflearn_Py/섹션 6/11. 수들의 조합/test.py
+++ Inflearn_Py/섹션 6/11. 수들의 조합/test.py
@@ -1,7 +1,33 @@
 import sys
-sys.stdin = open("test.txt", "r")
 
+sys.stdin = open("in4.txt", "r")
+
+
+def dfs(level, st):
+    global cnt
+
+    if level == K:
+        # print(combiantion)
+        if sum(combiantion) % M == 0:
+            cnt += 1
+        return
+
+    for i in range(st, len(samples)):
+        combiantion[level] = samples[i]
+        dfs(level + 1, i + 1)
 
 
 if __name__ == '__main__':
-    pass
+    N, K = map(int, input().split())
+    samples = list(map(int, input().split()))
+    M = int(input())
+    cnt = 0
+
+    combiantion = [0] * K
+    dfs(0, 0)
+
+    print(cnt)
+
+# 5 3
+# 2 4 5 8 12
+# 6
